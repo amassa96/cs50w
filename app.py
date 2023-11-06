ce7984bed325c3b4cbd7d9d051f2eb8ccb9c4cfc
Andrew Massa <amassa96@gmail.com>
2023-11-05 17:52:18 -0800

    merging app.py

--- app.py
+++ app.py
@@ -2,4 +2,8 @@
 # test from local
 #pull from remote
 #playing with saving in vscode
+<<<<<<< HEAD
 # adding text and then going to add text to the remote repostitory and then pull and then push
+=======
+# remote repository comment to be pulled
+>>>>>>> c2b7663b8dfc99c142bece8f785de9af0ed088be
