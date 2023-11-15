0026b35dd4f9cd4ec9c81aafd87b92b1395be54d
Andrew Massa <amassa96@gmail.com>
2023-11-14 19:23:23 -0800

    cleaning up directories

--- cs50w/week_0/app.py
+++
@@ -1,6 +0,0 @@
-# test from remote repository
-# test from local
-#pull from remote
-#playing with saving in vscode
-# adding text and then going to add text to the remote repostitory and then pull and then push
-# remote repository comment to be pulled
