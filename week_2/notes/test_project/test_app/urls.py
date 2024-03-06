f26503174acc9839d5a0439746490c66b7bdd499
amassa96 <42423492+amassa96@users.noreply.github.com>
2024-03-05 23:08:49 -0800

    Delete week_2 directory

--- week_2/notes/test_project/test_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "test_app"
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet")
-]
